d334a2c84ad56cee0beb2b676fb7d198ffeb80ef
Ioakeim Lazakis <ilazakis@gmail.com>
2016-09-23 23:46:14 +0100

    Updated test script to accommodate for resource copying

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+rm -rf /private/tmp/fixtures # copy fixtures/resources to make available to tests
 cp -r fixtures /private/tmp
-xcodebuild -scheme RequestBuilder test
+swift package generate-xcodeproj
+xcodebuild \ 
+	-scheme RequestBuilder \
+	-enableCodeCoverage YES \
+	-destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \
+	test
