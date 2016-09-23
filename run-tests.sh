a1eb01cb82ad265a4b701f1c2e1dbeaa329aae17
Joakim Lazakis <ilazakis@users.noreply.github.com>
2016-09-23 23:51:09 +0100

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 rm -rf /private/tmp/fixtures # copy fixtures/resources to make available to tests
 cp -r fixtures /private/tmp
 swift package generate-xcodeproj
-xcodebuild \ 
+xcodebuild \
 	-scheme RequestBuilder \
 	-enableCodeCoverage YES \
 	-destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \
